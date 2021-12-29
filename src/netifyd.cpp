f98d4fb02cd2af17486f0b0696e8ece7ef6fbce6
Darryl Sokoloski <darryl@sokoloski.ca>
2021-12-29 13:53:49 -0500

    Disabled faulty packet discard logic.
    Added tracking of TCP resets.
    Fixed tracking of TCP sequence errors.
    Quickly purge TCP flows after TCP FIN.

--- src/netifyd.cpp
+++ src/netifyd.cpp
@@ -1470,6 +1470,7 @@ static void nd_json_add_stats(json &parent,
     parent["ip"] = stats->pkt.ip;
     parent["tcp"] = stats->pkt.tcp;
     parent["tcp_seq_error"] = stats->pkt.tcp_seq_error;
+    parent["tcp_resets"] = stats->pkt.tcp_resets;
     parent["udp"] = stats->pkt.udp;
     parent["icmp"] = stats->pkt.icmp;
     parent["igmp"] = stats->pkt.igmp;
@@ -1503,14 +1504,19 @@ static void nd_json_process_flows(
 
         uint32_t last_seen = i->second->ts_last_seen / 1000;
         uint32_t ttl = (
+            i->second->ip_protocol != IPPROTO_TCP
+        ) ? nd_config.ttl_idle_flow : nd_config.ttl_idle_tcp_flow;
+/*
+        uint32_t ttl = (
             i->second->ip_protocol != IPPROTO_TCP || i->second->flags.tcp_fin.load()
         ) ? nd_config.ttl_idle_flow : nd_config.ttl_idle_tcp_flow;
-
+*/
 //        nd_dprintf("%s: Purge flow?  %lus old, ttl: %lu (%lu <? %lu)\n",
 //            i->second->iface->second.c_str(),
 //            now - last_seen, ttl, last_seen + ttl, now);
 
-        if (last_seen + ttl < now) {
+        if ((i->second->ip_protocol == IPPROTO_TCP && i->second->flags.tcp_fin.load()) ||
+            last_seen + ttl < now) {
 
 //            nd_dprintf("%s: Purge flow, %lus old, ttl: %lu.\n",
 //                i->second->iface->second.c_str(), now - last_seen, ttl);
